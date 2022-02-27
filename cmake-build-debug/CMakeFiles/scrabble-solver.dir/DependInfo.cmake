d3a7c219f1442d6c617ed6b866d70a3b05d77b95
Adam Escobedo <acescobedo@smu.edu>
2022-02-27 03:26:02 -0600

    Added Debug files

---
+++ cmake-build-debug/CMakeFiles/scrabble-solver.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/misc1/CLionProjects/scrabble-solver/BoardReader/BoardReader.cpp" "/mnt/c/Users/misc1/CLionProjects/scrabble-solver/cmake-build-debug/CMakeFiles/scrabble-solver.dir/BoardReader/BoardReader.cpp.o"
+  "/mnt/c/Users/misc1/CLionProjects/scrabble-solver/HandManager/HandManager.cpp" "/mnt/c/Users/misc1/CLionProjects/scrabble-solver/cmake-build-debug/CMakeFiles/scrabble-solver.dir/HandManager/HandManager.cpp.o"
+  "/mnt/c/Users/misc1/CLionProjects/scrabble-solver/main.cpp" "/mnt/c/Users/misc1/CLionProjects/scrabble-solver/cmake-build-debug/CMakeFiles/scrabble-solver.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/scrabble-solver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scrabble-solver.dir/BoardReader/BoardReader.cpp.o"
+  "CMakeFiles/scrabble-solver.dir/HandManager/HandManager.cpp.o"
+  "CMakeFiles/scrabble-solver.dir/main.cpp.o"
+  "scrabble-solver"
+  "scrabble-solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scrabble-solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
