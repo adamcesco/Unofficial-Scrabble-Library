d4594db5b5b4e03c0fc9373ba4ce3c3024ac8bc8
Adam Escobedo <acescobedo@smu.edu>
2022-03-18 00:20:47 -0500

    Fixed file routing

--- ScrabbleDataset/ScrabbleDataset.h
+++ ScrabbleDataset/ScrabbleDataset.h
@@ -1,7 +1,7 @@
 #ifndef SCRABBLE_SOLVER_SCRABBLEDATASET_H
 #define SCRABBLE_SOLVER_SCRABBLEDATASET_H
 
-#include "../LString/TString.h"
+#include "../TString/TString.h"
 
 struct AnchoredString{
     AnchoredString(const string& str1, int subscript){ first = str1; second = subscript; }
